5a1a087c9eac99853d2de0508d8ba98f9c6f7a7b
alvaro-andrade <alvaro.andrade@salesforce.com>
2019-02-21 11:09:45 -0800

    First Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "./style/main.less";
+
+class Welcome extends Component {
+    render () {
+        return <h1>Hello World from React boilerplate</h1>;
+    }
+}
+
+ReactDOM.render(<Welcome />, document.getElementById('root'));
